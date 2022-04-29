edc885b30faa0dde4a206ad4ed7f1d922018c9eb
CheongWoong <cwkang0204@unist.ac.kr>
2022-04-29 15:46:33 +0900

    update

---
+++ src/hyukdong/gradient_method.py
@@ -0,0 +1,34 @@
+
+import torch 
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+
+################################################################################
+### Inputs
+# target_class: (int tensor) target class index.
+# M: (int) the number of samples (interpolation, noise, ...).
+# sigma: (int) standard deviation for gaussian noise.
+################################################################################
+### Outputs
+# output: dictionary (output['attribution'] must be provided as aligned with the input x.)
+################################################################################
+
+
+def integrated_gradient(model, x, x_baseline, target_class, M, **temp):
+    output = {}
+
+    return output
+
+
+def vanila_gradient(model, x, **temp):
+    output = {}
+    
+    return output
+
+
+def smooth_gradient(model, x, target_class, M, sigma, **temp):
+    output = {}
+    
+    return output
+
